2c3f1c1bbe8b200efa3f7fd7279c02947a23aff1
daniel <dgw@posteo.net>
2020-02-18 13:40:14 -0600

    added species

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 use std::fmt;
 use std::str::FromStr;
 
+
+#[derive(Debug)]
+pub enum Error {
+    InvalidChainError,
+    InvalidSpeciesError,
+}
+
 #[derive(Debug, Clone)]
 pub enum Chain {
     TRA,
@@ -18,24 +25,24 @@ impl Chain {
     }
 }
 
-#[derive(Debug)]
-pub enum Error {
-    InvalidChainError,
-}
-
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Invalid chain. Possible values are TRA, TRB, TRD, TRG, IGH, IGK, IGL."
-        )
+        match self {
+            Error::InvalidChainError => write!(
+                f,
+                "Invalid chain. Possible values are TRA, TRB, TRD, TRG, IGH, IGK, IGL."
+            ),
+            Error::InvalidSpeciesError => {
+                write!(f, "Invalid species. Possible values are human or mouse")
+            }
+        }
     }
 }
 
 impl FromStr for Chain {
     type Err = Error;
 
-    fn from_str(s: &str) -> Result<Chain, Self::Err> {
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
         let s = s.to_ascii_uppercase();
         let variant = match s.as_ref() {
             "TRA" => Chain::TRA,
@@ -61,6 +68,43 @@ impl fmt::Display for Chain {
             Chain::IGH => "IGH",
             Chain::IGK => "IGK",
             Chain::IGL => "IGL",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+
+#[derive(Debug, Clone)]
+pub enum Species {
+    Human,
+    Mouse,
+}
+
+impl Species {
+    pub fn enumerated() -> &'static [&'static str] {
+        &["human", "mouse"]
+    }
+}
+
+impl FromStr for Species {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.to_ascii_uppercase();
+        let variant = match s.as_ref() {
+            "HUMAN" | "H" => Species::Human,
+            "MOUSE" | "M" => Species::Mouse,
+            _ => return Err(Error::InvalidSpeciesError),
+        };
+        Ok(variant)
+    }
+}
+
+impl fmt::Display for Species {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let s = match *self {
+            Species::Human => "human",
+            Species::Mouse => "mouse"
         };
         write!(f, "{}", s)
     }
